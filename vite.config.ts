ddad3dfede496e755f2e0807e90e4c50c36ee656
thisishakem <thisishakemdar@gmail.com>
2025-02-02 05:59:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // مهم لـ Vercel
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
